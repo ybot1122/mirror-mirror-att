b6efa5976d6eca0a9e104713aa46ee4d299c739a
Adam Stelle <adamstelle@gmail.com>
2016-07-30 15:55:37 -0700

    Merging

--- public/javascripts/twitter.js
+++ public/javascripts/twitter.js
@@ -1,9 +1,3 @@
-/**
-* Use this snippet with your favorite
-* JavaScript client frameworks.
-* Resource: https://api.jquery.com/jquery.post/
-*/
-
 const data = {
     query: 'query { searchTwitterTweets(q: "Trump", count: 5, result_type:popular) { id created_at text }  }   ',
     variables: ""
@@ -14,8 +8,13 @@ $.ajax({
     url: "https://api.scaphold.io/graphql/7db5b0fd-0970-42d1-97d0-49b2fe88f364",
     data: data,
     success: function(result) {
-        console.log("That was easy!", result);
-        return result;
+        var tweets = result["data"]["searchTwitterTweets"];
+        console.log(tweets);
+        var output;
+        for each(i in tweets) {
+          output.push('"'+i["text"]+'');
+        }
+        $("#bottom").append(output);
     },
     error: function(xhr, ajaxOptions, error) {
         console.log("That didn't go so well.");
