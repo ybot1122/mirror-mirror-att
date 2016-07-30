ad3c2a580790d3d9a47966a21efc1ab0850def59
Toby Liu <ybot1122@gmail.com>
2016-07-30 12:30:39 -0700

    initial commit of nuance package

---
+++ nuance/configuration.js
@@ -0,0 +1,17 @@
+// Credentials
+
+var URL = 'wss://ws.dev.nuance.com/?';
+
+var APP_ID = "NMDPTRIAL_ybot1122_gmail_com20160730134413";
+var APP_KEY = "14879ed878c20fb794c7d248cb643378c5792b83671db679de3e22d4fc338c10ba2be932edf8e981bfbbc98a20fc03e5752e68dc4b1f552a5d78fe59ab8a6965";
+var USER_ID = "vjsisneros@gmail.com";
+var NLU_TAG = "M3268_A1663_V1";
+
+// ASR
+// See: https://developer.nuance.com/public/index.php?task=supportedLanguages
+var ASR_LANGUAGE = "eng-USA";
+
+// TTS
+// See: https://developer.nuance.com/public/index.php?task=supportedLanguages
+var TTS_LANGUAGE = "eng-USA";
+var TTS_VOICE = "";
