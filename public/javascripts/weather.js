ccff6331a4dcfc0067ba09571a5ebc755fa6bb5a
Adam Stelle <adamstelle@gmail.com>
2016-08-29 13:41:42 -0400

    Add geolocation to get local weather

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -1,17 +1,30 @@
 (function() {
  $(document).ready(function() {
    var clientID = '1aea91f901544d2a';
-   var zip = '98101';
    var currentWeather = $('#currentWeather');
    var $temp = $('#temp');
    var forecast = $('#forecast');
    var currentTime = moment()
    var night;
    var socket = io();
-   var weatherEndpoint = 'https://api.wunderground.com/api/'+clientID+'/conditions/q/'+zip+'.json';
-   var forecastEndpoint = 'https://api.wunderground.com/api/'+clientID+'/forecast10day/q/'+zip+'.json';
-   var sunTimes = 'https://api.wunderground.com/api/'+clientID+'/astronomy/q/'+zip+'.json';
+   var zip;
 
+  //  Get users location
+  if ("geolocation" in navigator) {
+    console.log("geolocation is available");
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var locationJSON = $.getJSON("http://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude+"", function(data) {
+        var locAddress = data.results[0].address_components;
+        zip = (locAddress[locAddress.length - 1].long_name).toString();
+        console.log(zip);
+        getWeather(zip);
+      });
+    });
+  } else {
+    console.log("geolocation is NOT available");
+    zip = '98101';
+    getWeather(zip);
+  }
 
    function getIcon(iconName, night) {
      var weatherIconsDay = {
@@ -67,54 +80,58 @@
      $("#topRightCorner").removeClass("hidden");
    });
 
-   $.ajax({
-     type: 'GET',
-     dataType: 'jsonp',
-     url: weatherEndpoint
-   })
-   .then(function(weatherResponse) {
+   function getWeather(zip) {
+     var weatherEndpoint = 'https://api.wunderground.com/api/'+clientID+'/conditions/q/'+zip+'.json';
+     var forecastEndpoint = 'https://api.wunderground.com/api/'+clientID+'/forecast10day/q/'+zip+'.json';
+     var sunTimes = 'https://api.wunderground.com/api/'+clientID+'/astronomy/q/'+zip+'.json';
      $.ajax({
        type: 'GET',
        dataType: 'jsonp',
-       url: forecastEndpoint
+       url: weatherEndpoint
      })
-     .then(function(forecastResponse) {
+     .then(function(weatherResponse) {
        $.ajax({
          type: 'GET',
          dataType: 'jsonp',
-         url: sunTimes
+         url: forecastEndpoint
        })
-       .then(function(output) {
-         var sunsetHour = parseInt(output["sun_phase"]["sunset"]["hour"]);
-         var sunsetMinute = parseInt(output["sun_phase"]["sunset"]["minute"]);
-         var sunriseHour = parseInt(output["sun_phase"]["sunrise"]["hour"]);
-         var sunriseMinute = parseInt(output["sun_phase"]["sunrise"]["minute"]);
-         var currentHour = parseInt(output["moon_phase"]["current_time"]["hour"]);
-         var currentMinute = parseInt(output["moon_phase"]["current_time"]["minute"]);
-         if ((currentHour > sunsetHour || (currentHour == currentMinute && currentMinute > sunsetMinute)) ||  (currentHour < sunriseHour || (currentHour == sunriseHour && currentMinute < sunriseMinute))) {
-           night = "true";
-         } else {
-           night = "false";
-         }
+       .then(function(forecastResponse) {
+         $.ajax({
+           type: 'GET',
+           dataType: 'jsonp',
+           url: sunTimes
+         })
+         .then(function(output) {
+           var sunsetHour = parseInt(output["sun_phase"]["sunset"]["hour"]);
+           var sunsetMinute = parseInt(output["sun_phase"]["sunset"]["minute"]);
+           var sunriseHour = parseInt(output["sun_phase"]["sunrise"]["hour"]);
+           var sunriseMinute = parseInt(output["sun_phase"]["sunrise"]["minute"]);
+           var currentHour = parseInt(output["moon_phase"]["current_time"]["hour"]);
+           var currentMinute = parseInt(output["moon_phase"]["current_time"]["minute"]);
+           if ((currentHour > sunsetHour || (currentHour == currentMinute && currentMinute > sunsetMinute)) ||  (currentHour < sunriseHour || (currentHour == sunriseHour && currentMinute < sunriseMinute))) {
+             night = "true";
+           } else {
+             night = "false";
+           }
 
-         var $current = weatherResponse.current_observation;
-        //  var icon = getIcon($current.icon, night);
-         currentWeather.load(getIcon($current.icon, night));
+           var $current = weatherResponse.current_observation;
+          //  var icon = getIcon($current.icon, night);
+           currentWeather.load(getIcon($current.icon, night));
 
+           var currentTemp = $current.temp_f;
+           $temp.append(currentTemp + "°");
 
-         var currentTemp = $current.temp_f;
-         $temp.append(currentTemp + "°");
-
-         var $forecast = [];
-         for (var i=0; i<5; i++) {
-           var daily = forecastResponse.forecast.simpleforecast.forecastday[i];
-           $('#day'+[i]+' > .icon').load(getIcon(daily.icon, "false"));
-           $('#day'+[i]+' > .highTemp').append(daily.high.fahrenheit+"°");
-           $('#day'+[i]+' > .lowTemp').append(daily.low.fahrenheit+"°");
-           $('#day'+[i]+' > .dayOfWeek').append((daily.date.weekday).substring(0, 3));
-         }
+           var $forecast = [];
+           for (var i=0; i<5; i++) {
+             var daily = forecastResponse.forecast.simpleforecast.forecastday[i];
+             $('#day'+[i]+' > .icon').load(getIcon(daily.icon, "false"));
+             $('#day'+[i]+' > .highTemp').append(daily.high.fahrenheit+"°");
+             $('#day'+[i]+' > .lowTemp').append(daily.low.fahrenheit+"°");
+             $('#day'+[i]+' > .dayOfWeek').append((daily.date.weekday).substring(0, 3));
+           }
+         });
        });
      });
-   });
+   }
  });
 })();
